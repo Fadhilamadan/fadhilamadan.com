367a6ec5393c4079f2a7d18ec6682a8572368d3c
Fadhil Amadan <fadhil.amadan@yahoo.co.id>
2021-04-05 20:44:09 +0700

    added: nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "../styles/globals.css";
 
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
