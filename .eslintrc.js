40727405286395b0a0eca74d48acf83737bfade5
Fadhil Amadan <fadhil.amadan@yahoo.co.id>
2021-04-05 18:57:11 +0700

    added: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  extends: [
+    "plugin:prettier/recommended",
+    "prettier/react",
+    "kentcdodds",
+    "kentcdodds/react",
+    "kentcdodds/jsx-a11y",
+  ],
+
+  plugins: ["prettier", "simple-import-sort"],
+
+  rules: {
+    // simple import rules
+    "simple-import-sort/exports": "warn",
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        groups: [
+          ["^.+\\.s?css$"],
+          ["^\\u0000"],
+          ["^react$"],
+          ["^@/"],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+        ],
+      },
+    ],
+
+    // react rules
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/no-unstable-nested-components": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/jsx-sort-props": [
+      "warn",
+      {
+        reservedFirst: ["key"],
+      },
+    ],
+
+    // various rules
+    "no-void": "off",
+    "require-await": "off",
+    "max-lines-per-function": "off",
+  },
+};
