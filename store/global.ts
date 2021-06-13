45b8385a808bb4d3a0d4d2582559808b86a57856
Fadhil Amadan <git@fadhilamadan.com>
2021-06-13 21:32:35 +0700

    feat(store): meta and global

---
+++ store/global.ts
@@ -0,0 +1,30 @@
+import { useCallback } from 'react';
+
+import create, { State } from 'zustand';
+import shallow from 'zustand/shallow';
+
+interface GlobalStore extends State {
+  isMobileDrawerOpen: boolean;
+  toggleMobileDrawer: (state?: unknown) => void;
+}
+
+export const useGlobalStore = create<GlobalStore>((set, get) => ({
+  isMobileDrawerOpen: false,
+  toggleMobileDrawer: (isMobileDrawerOpen = !get().isMobileDrawerOpen) => {
+    set({ isMobileDrawerOpen: Boolean(isMobileDrawerOpen) });
+  },
+}));
+
+export function useMobileDrawer() {
+  const [isOpen, onToggle] = useGlobalStore(
+    (store) => [store.isMobileDrawerOpen, store.toggleMobileDrawer],
+    shallow,
+  );
+
+  return {
+    isOpen,
+    onClose: useCallback(() => onToggle(false), []),
+    onOpen: useCallback(() => onToggle(true), []),
+    onToggle,
+  };
+}
