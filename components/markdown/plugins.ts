cc10796845d7401fb3e373444cc0f49f4c7a5500
Fadhil Amadan <git@fadhilamadan.com>
2021-06-14 10:16:02 +0700

    refactor(component): prettier markdown

--- components/markdown/plugins.ts
+++ components/markdown/plugins.ts
@@ -1,6 +1,6 @@
-import a11yEmoji from "@fec/remark-a11y-emoji";
-import slug from "remark-slug";
-import toc from "remark-toc";
+import a11yEmoji from '@fec/remark-a11y-emoji';
+import slug from 'remark-slug';
+import toc from 'remark-toc';
 
 export const postPlugins = [
   a11yEmoji,
