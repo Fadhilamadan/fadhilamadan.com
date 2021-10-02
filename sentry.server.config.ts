8ea1de688ac6b4657c14b72ce3a6d1e141ce071a
Fadhil Amadan <git@fadhilamadan.com>
2021-10-02 10:52:19 +0700

    chore(sentry): add debug and environment preview

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,8 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  environment: process.env.NODE_ENV,
-  dsn: process.env.SENTRY_DSN,
+  debug: true,
   tracesSampleRate: 1.0,
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.IS_PREVIEW ? 'preview' : process.env.NODE_ENV,
 });
