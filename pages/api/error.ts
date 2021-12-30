443504fc76744e8e3d8833cbe5ee7955f57080e6
Fadhil Amadan <git@fadhilamadan.com>
2021-12-30 21:06:33 +0700

    refactor(sentry): delete unused config sentry

--- pages/api/error.ts
+++
@@ -1,9 +0,0 @@
-import { withSentry } from '@sentry/nextjs';
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  throw new Error('API throw error test');
-  res.status(200).json({ name: 'Fadhil Amadan' });
-};
-
-export default withSentry(handler);
